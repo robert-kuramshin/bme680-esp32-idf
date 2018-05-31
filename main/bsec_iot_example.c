e859ef0041946cde253bbeea20419c5294044b17
robert-kuramshin <robert.kuramshin@gatech.edu>
2018-05-31 19:13:56 -0400

    getting started on basic functionality

--- main/bsec_iot_example.c
+++ main/bsec_iot_example.c
@@ -74,8 +74,30 @@
 /**********************************************************************************************************************/
 /* header files */
 /**********************************************************************************************************************/
-
 #include "bsec_integration.h"
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include <sys/time.h>
+#include <string.h>
+
+#include "driver/i2c.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+
+/**********************************************************************************************************************/
+/* defines */
+/**********************************************************************************************************************/
+
+#define I2C_ACK  0x0
+#define I2C_NACK 0x1
+
+#define I2C_BUS       0
+#define I2C_SCL       14
+#define I2C_SDA       13
+#define I2C_FREQ      100000
 
 /**********************************************************************************************************************/
 /* functions */
@@ -93,10 +115,24 @@
  */
 int8_t bus_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data_ptr, uint16_t data_len)
 {
-    // ...
-    // Please insert system specific function to write to the bus where BME680 is connected
-    // ...
-    return 0;
+    i2c_cmd_hande_t cmd = 92c_cmd_link_create();
+
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev_addr) << 1 | I2C_MASTER_WRITE, ACK_CHECK_EN);
+
+    if (reg_addr){
+        i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+    }
+
+    if (reg_data_ptr){
+        i2c_master_write(cmd, reg_data_ptr, data_len, ACK_CHECK_EN);        
+    }
+
+    i2c_master_stop(cmd);
+    esp_err_t res = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    
+    return res;
 }
 
 /*!
@@ -111,10 +147,27 @@ int8_t bus_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data_ptr, uint
  */
 int8_t bus_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data_ptr, uint16_t data_len)
 {
-    // ...
-    // Please insert system specific function to read from bus where BME680 is connected
-    // ...
-    return 0;
+    if (!data_len){
+        return 1;
+    }
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+    if (!reg_data_ptr)
+        i2c_master_stop(cmd);
+    
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_READ, ACK_CHECK_EN);
+    if (data_len > 1) i2c_master_read(cmd, reg_data_ptr, data_len-1, I2C_ACK_VAL);
+    i2c_master_read_byte(cmd, reg_data_ptr + data_len-1, I2C_NACK_VAL);
+    i2c_master_stop(cmd);
+    
+    esp_err_t res = i2c_master_cmd_begin(0, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+
+    return res;
 }
 
 /*!
@@ -126,9 +179,7 @@ int8_t bus_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data_ptr, uint1
  */
 void sleep(uint32_t t_ms)
 {
-    // ...
-    // Please insert system specific function sleep or delay for t_ms milliseconds
-    // ...
+    vTaskDelay(t_ms / portTICK_RATE_MS);
 }
 
 /*!
@@ -138,11 +189,9 @@ void sleep(uint32_t t_ms)
  */
 int64_t get_timestamp_us()
 {
-    int64_t system_current_time = 0;
-    // ...
-    // Please insert system specific function to retrieve a timestamp (in microseconds)
-    // ...
-    return system_current_time;
+    struct timeval time;
+    gettimeofday(&time,0);
+    return time.tv_sec*1e6 + time.tv_usec;
 }
 
 /*!
@@ -164,9 +213,7 @@ int64_t get_timestamp_us()
 void output_ready(int64_t timestamp, float iaq, uint8_t iaq_accuracy, float temperature, float humidity,
      float pressure, float raw_temperature, float raw_humidity, float gas, bsec_library_return_t bsec_status)
 {
-    // ...
-    // Please insert system specific code to further process or display the BSEC outputs
-    // ...
+    printf("%d | temp: %f.2 | hum: %f | pres: %f.2 | iaq: %f \n",timestamp,temperature,humidity,pressure,iaq)
 }
 
 /*!
@@ -228,29 +275,37 @@ uint32_t config_load(uint8_t *config_buffer, uint32_t n_buffer)
  *
  * @return      result of the processing
  */
-int main()
+int app_main()
 {
     return_values_init ret;
     
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_SDA_PIN;
+    conf.scl_io_num = I2C_SCL_PIN;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_FREQ;
+    i2c_param_config(0, &conf);
+    i2c_driver_install(0, I2C_MODE_MASTER, 0, 0, 0);
+
+
     /* Call to the function which initializes the BSEC library 
      * Switch on low-power mode and provide no temperature offset */
     ret = bsec_iot_init(BSEC_SAMPLE_RATE_LP, 0.0f, bus_write, bus_read, sleep, state_load, config_load);
     if (ret.bme680_status)
     {
-        /* Could not intialize BME680 */
-        return (int)ret.bme680_status;
+        printf("Could not intialize BME680\n");
     }
     else if (ret.bsec_status)
     {
-        /* Could not intialize BSEC library */
-        return (int)ret.bsec_status;
+        printf("Could not intialize BSEC library \n");
     }
     
     /* Call to endless loop function which reads and processes data based on sensor settings */
     /* State is saved every 10.000 samples, which means every 10.000 * 3 secs = 500 minutes  */
     bsec_iot_loop(sleep, get_timestamp_us, output_ready, state_save, 10000);
-    
-    return 0;
+
 }
 
 /*! @}*/
